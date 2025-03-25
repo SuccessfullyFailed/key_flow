7b5b851276941923a5abc4eae9e79f1f51499d82
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-03-25 14:36:03 +0100

    U256 use literal file allocation instead of new function

--- src/u256.rs
+++ src/u256.rs
@@ -1,4 +1,4 @@
-use std::ops::{ Add, BitAnd, BitOr, BitXor, Mul, Shl, Shr };
+use std::ops::{ Add, BitAnd, BitOr, BitXor, Mul, Not, Shl, Shr };
 
 
 
@@ -19,7 +19,7 @@ impl U256 {
 
 	/// Create a new empty u128 integer.
 	pub const fn zero() -> U256 {
-		U256::new(0, 0)
+		U256 { high: 0, low: 0 }
 	}
 
 
@@ -30,14 +30,14 @@ impl U256 {
 	pub fn overflowing_add(self, rhs:U256) -> (U256, bool) {
 		let (low, carry) = self.low.overflowing_add(rhs.low);
 		let (high, high_carry) = self.high.overflowing_add(rhs.high + (carry as u128));
-		(U256::new(high, low), high_carry)
+		(U256 { high, low }, high_carry)
 	}
 
 	/// Multiply two U256s. Returns new number and boolean indicating overflow.
 	pub fn overflowing_mul(self, rhs:U256) -> (U256, bool) {
 		let (low, carry) = self.low.overflowing_mul(rhs.low);
 		let high = self.high * rhs.low + self.low * rhs.high + carry as u128;
-		(U256::new(high, low), high > 0)
+		(U256 { high, low }, high > 0)
 	}
 }
 impl Default for U256 {
@@ -64,9 +64,9 @@ impl Shl<u64> for U256 {
 
 	fn shl(self, shift:u64) -> U256 {
 		if shift >= 128 {
-			U256::new(self.low << (shift - 128), 0)
+			U256{ high: self.low << (shift - 128), low: 0 }
 		} else {
-			U256::new((self.high << shift) | (self.low >> (128 - shift)), self.low << shift)
+			U256{ high: (self.high << shift) | (self.low >> (128 - shift)), low: self.low << shift }
 		}
 	}
 }
@@ -75,9 +75,9 @@ impl Shr<u64> for U256 {
 
 	fn shr(self, shift:u64) -> U256 {
 		if shift >= 128 {
-			U256::new(0, self.high >> (shift - 128))
+			U256 { high: 0, low: self.high >> (shift - 128) }
 		} else {
-			U256::new(self.high >> shift, (self.low >> shift) | (self.high << (128 - shift)))
+			U256 { high: self.high >> shift, low: (self.low >> shift) | (self.high << (128 - shift)) }
 		}
 	}
 }
@@ -85,20 +85,27 @@ impl BitAnd for U256 {
 	type Output = U256;
 
 	fn bitand(self, compare:U256) -> U256 {
-		U256::new(self.high & compare.high, self.low & compare.low)
+		U256 { high: self.high & compare.high, low: self.low & compare.low }
 	}
 }
 impl BitOr for U256 {
 	type Output = U256;
 
 	fn bitor(self, compare:U256) -> U256 {
-		U256::new(self.high | compare.high, self.low | compare.low)
+		U256 { high: self.high | compare.high, low: self.low | compare.low }
 	}
 }
 impl BitXor for U256 {
 	type Output = U256;
 	
 	fn bitxor(self, compare:U256) -> U256 {
-		U256::new(self.high ^ compare.high, self.low ^ compare.low)
+		U256 { high: self.high ^ compare.high, low: self.low ^ compare.low }
+	}
+}
+impl Not for U256 {
+	type Output = U256;
+
+	fn not(self) -> Self::Output {
+		U256 { high: !self.high, low: !self.low }
 	}
 }
