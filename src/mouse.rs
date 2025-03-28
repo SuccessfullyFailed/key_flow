8c8d0d6dc589ad6571873def53176481da3f751e
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-03-28 12:52:29 +0100

    Add mouse drag method

--- src/mouse.rs
+++ src/mouse.rs
@@ -1,5 +1,6 @@
 use winapi::{ shared::windef::POINT, um::winuser::{ mouse_event, GetCursorPos, SetCursorPos } };
 use crate::{ key::KeyPressDuration, keys::LBUTTON };
+use std::thread::sleep;
 
 
 
@@ -26,4 +27,13 @@ pub fn click<T>(position:[i32; 2], duration:T) where T:KeyPressDuration + 'stati
 	move_to(position);
 	LBUTTON.send_await(duration);
 	move_to(original_pos);
+}
+
+/// Drag the mouse from one point to another.
+pub fn drag<T>(start:[i32; 2], end:[i32; 2], press_duration:T) where T:KeyPressDuration + 'static + Send + Sync {
+	move_to(start);
+	LBUTTON.press();
+	sleep(press_duration.as_duration());
+	move_to(end);
+	LBUTTON.release();
 }
