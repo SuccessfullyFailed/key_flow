9863a563a8b026a5235eb6c2740dfaabab32e66d
SuccessfullyFailed <renzogerritzen@gmail.com>
2026-02-12 01:37:56 +0100

    Revamp mouse controls to use input-builder

--- src/input_builder.rs
+++ src/input_builder.rs
@@ -1,12 +1,36 @@
-use winapi::um::winuser::{ INPUT, INPUT_MOUSE, KEYBDINPUT, KEYEVENTF_KEYUP, MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP, MOUSEEVENTF_MIDDLEDOWN, MOUSEEVENTF_MIDDLEUP, MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_RIGHTUP, MOUSEEVENTF_XDOWN, MOUSEEVENTF_XUP, MOUSEINPUT, MapVirtualKeyW, SendInput };
+use cachew::cache;
+use winapi::um::winuser::{ GetSystemMetrics, INPUT, INPUT_MOUSE, KEYBDINPUT, KEYEVENTF_KEYUP, MOUSEEVENTF_ABSOLUTE, MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP, MOUSEEVENTF_MIDDLEDOWN, MOUSEEVENTF_MIDDLEUP, MOUSEEVENTF_MOVE, MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_RIGHTUP, MOUSEEVENTF_VIRTUALDESK, MOUSEEVENTF_XDOWN, MOUSEEVENTF_XUP, MOUSEINPUT, MapVirtualKeyW, SM_CXSCREEN, SM_CYSCREEN, SendInput };
 use crate::{ Key, KeyPattern, key_hook::handle_virtual_key_alteration, sleep };
 use std::{ mem, ptr, thread, time::Duration };
 
 
 
 #[derive(Clone)]
+enum InputBuilderInput {
+	KeyInput((INPUT, u8, bool)),
+	MouseInput(INPUT),
+	Delay(u64)
+}
+impl InputBuilderInput {
+	fn delay(&self) -> u64 {
+		match self {
+			InputBuilderInput::Delay(delay) => *delay,
+			_ => 0
+		}
+	}
+	fn raw_input(&self) -> Option<&INPUT> {
+		match self {
+			InputBuilderInput::KeyInput((input, _, _)) => Some(input),
+			InputBuilderInput::MouseInput(input) => Some(input),
+			InputBuilderInput::Delay(_) => None
+		}
+	}
+}
+
+
+#[derive(Clone)]
 pub struct InputBuilder {
-	inputs:Vec<(INPUT, u64, u8, bool)>
+	inputs:Vec<InputBuilderInput>
 }
 impl InputBuilder {
 
@@ -22,6 +46,12 @@ impl InputBuilder {
 
 
 	/* INPUT BUILDER METHODS */
+
+	/// Return self with a delay. All added actions after this one will happen after the delay.
+	pub fn with_delay(mut self, duration_millis:u64) -> Self {
+		self.add_delay(duration_millis);
+		self
+	}
 
 	/// Return self with an additional key-press input.
 	pub fn with_press(mut self, key:&dyn KeyOrKeyPattern) -> Self {
@@ -47,9 +77,21 @@ impl InputBuilder {
 		self
 	}
 
-	/// Return self with addition key-send inputs to type a specific str.
+	/// Return self with additional key-send inputs to type a specific str.
 	pub fn with_send_str(mut self, string:&str, press_duration:u64) -> Self {
 		self.add_send_str(string, press_duration);
+		self
+	}
+	
+	/// Return self with an additional mouse displacement.
+	pub fn with_mouse_displacement(mut self, offset:[i32; 2]) -> Self {
+		self.add_mouse_displacement(offset);
+		self
+	}
+
+	/// Return self with an additional mouse movement.
+	pub fn with_mouse_move(mut self, target_position:[i32; 2]) -> Self {
+		self.add_mouse_move(target_position);
 		self
 	}
 
@@ -57,14 +99,19 @@ impl InputBuilder {
 
 	/* INPUT ADDITION METHODS */
 
+	/// Add a delay. All added actions after this one will happen after the delay.
+	pub fn add_delay(&mut self, duration_millis:u64) {
+		self.inputs.push(InputBuilderInput::Delay(duration_millis));
+	}
+
 	/// Add a key-press input.
 	pub fn add_press(&mut self, key:&dyn KeyOrKeyPattern) {
-		self.add_raw_inputs(key, true, 0);
+		self.add_raw_key_inputs(key, true);
 	}
 
 	/// Add a key-release input.
 	pub fn add_release(&mut self, key:&dyn KeyOrKeyPattern) {
-		self.add_raw_inputs(key, false, 0);
+		self.add_raw_key_inputs(key, false);
 	}
 
 	/// Add a key-click input. Presses it and immediately releases it.
@@ -75,8 +122,9 @@ impl InputBuilder {
 
 	/// Add a key-send input. Presses it and releases it later.
 	pub fn add_send(&mut self, key:&dyn KeyOrKeyPattern, duration_millis:u64) {
-		self.add_raw_inputs(key, true, duration_millis);
-		self.add_raw_inputs(key, false, duration_millis);
+		self.add_raw_key_inputs(key, true);
+		self.add_delay(duration_millis);
+		self.add_raw_key_inputs(key, false);
 	}
 
 	/// Add key-send inputs to type a specific str.
@@ -86,6 +134,21 @@ impl InputBuilder {
 			self.add_send(&key_pattern, press_duration);
 		}
 	}
+	
+	/// Add a mouse displacement input.
+	pub fn add_mouse_displacement(&mut self, offset:[i32; 2]) {
+		self.add_raw_mouse_input(MOUSEEVENTF_MOVE, offset[0], offset[1])
+	}
+
+	/// Add a mouse move input.
+	pub fn add_mouse_move(&mut self, target_position:[i32; 2]) {
+		const POSITION_MULTIPLIER:i32 = 65535;
+
+		let screen_size:&[i32; 2] = cache!([i32; 2], [GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN)]);
+		let normalized_position:[i32; 2] = [target_position[0] * POSITION_MULTIPLIER / screen_size[0], target_position[1] * POSITION_MULTIPLIER / screen_size[1]];
+		self.add_raw_mouse_input(MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_VIRTUALDESK, normalized_position[0], normalized_position[1]);
+	}
+	
 
 
 
@@ -103,15 +166,18 @@ impl InputBuilder {
 		while cursor < self.inputs.len() {
 
 			// Group all inputs that are scheduled to execute at the same time.
-			let next_delay_index:usize = self.inputs.iter().skip(cursor).position(|(_, delay, _, _)| *delay != 0).unwrap_or(self.inputs.len() - 1);
-			let execution_group:Vec<&(INPUT, u64, u8, bool)> = self.inputs.iter().skip(cursor).take(next_delay_index + 1).collect();
-			let delay_after_execution:u64 = execution_group.last().map(|last| last.1).unwrap_or_default();
-			let inputs_to_execute:Vec<INPUT> = execution_group.iter().map(|(input, _, _, _)| input.clone()).collect();
+			let next_delay_index:usize = self.inputs.iter().skip(cursor).position(|input| input.delay() != 0).unwrap_or(self.inputs.len() - 1);
+			let execution_group:Vec<&InputBuilderInput> = self.inputs.iter().skip(cursor).take(next_delay_index + 1).collect();
+			let delay_after_execution:u64 = execution_group.last().map(|last| last.delay()).unwrap_or_default();
+			let inputs_to_execute:Vec<INPUT> = execution_group.iter().map(|input| input.raw_input()).flatten().cloned().collect();
 			Self::execute_inputs(inputs_to_execute);
 
 			// Update virtual key-states.
-			for (_, _, key, down) in execution_group {
-				handle_virtual_key_alteration(*key, *down);
+			for input in execution_group {
+				match input {
+					InputBuilderInput::KeyInput((_, key, down)) => handle_virtual_key_alteration(*key, *down),
+					_ => {}
+				}
 			}
 
 			// Sleep any possible delays.
@@ -133,53 +199,52 @@ impl InputBuilder {
 
 	/* RAW INPUT METHODS */
 
-	/// Add an input from core data.
-	fn add_raw_inputs(&mut self, key:&dyn KeyOrKeyPattern, keys_down:bool, delay:u64) {
+	/// Add an input from core key-data.
+	#[allow(invalid_value)]
+	fn add_raw_key_inputs(&mut self, key:&dyn KeyOrKeyPattern, keys_down:bool) {
+		const MOUSE_DOWN_EVENTS:[u32; 5] = [MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_MIDDLEDOWN, MOUSEEVENTF_XDOWN, MOUSEEVENTF_XDOWN];
+		const MOUSE_UP_EVENTS:[u32; 5] = [MOUSEEVENTF_LEFTUP, MOUSEEVENTF_RIGHTUP, MOUSEEVENTF_MIDDLEUP, MOUSEEVENTF_XUP, MOUSEEVENTF_XUP];
+
 		self.inputs.extend(
-			key.as_pattern().keys().into_iter().map(|key| (
-				Self::create_raw_input(key.key_code(), keys_down),
-				delay,
-				key.key_code(),
-				keys_down
-			)).collect::<Vec<(INPUT, u64, u8, bool)>>()
+			key.as_pattern().keys().into_iter().map(|key| {
+				let key_code:u8 = key.key_code();
+				InputBuilderInput::KeyInput((
+					unsafe {
+						if key_code < 6 {
+							let mut input_record:INPUT = INPUT { type_: INPUT_MOUSE, u: mem::MaybeUninit::uninit().assume_init() };
+							let event_list:&[u32; 5] = if keys_down { &MOUSE_DOWN_EVENTS } else { &MOUSE_UP_EVENTS };
+							let flags:u32 = event_list[key_code as usize - 1];
+							let input:MOUSEINPUT = MOUSEINPUT { dx: 0, dy: 0, mouseData: 0, dwFlags: flags, time: 0, dwExtraInfo: 0 };
+							ptr::write(&mut input_record.u as *mut _ as *mut MOUSEINPUT, input);
+							input_record
+						} else {
+							let mut input_record:INPUT = INPUT { type_: 1, u: mem::MaybeUninit::uninit().assume_init() };
+							let flags:u32 = if keys_down { 0 } else { KEYEVENTF_KEYUP };
+							let input:KEYBDINPUT = KEYBDINPUT { wVk: key_code as u16, wScan: MapVirtualKeyW(key_code as u32, 0) as u16, dwFlags: flags, time: 0, dwExtraInfo: 0 };
+							ptr::write(&mut input_record.u as *mut _ as *mut KEYBDINPUT, input);
+							input_record
+						}
+					},
+					key.key_code(),
+					keys_down
+				))
+			}).collect::<Vec<InputBuilderInput>>()
 		);
 	}
 
-	/// Create a raw input from core data.
-	fn create_raw_input(key_code:u8, key_down:bool) -> INPUT {
-		if key_code < 6 {
-			Self::create_raw_input_mouse(key_code, key_down)
-		} else {
-			Self::create_raw_input_keyboard(key_code, key_down)
-		}
-	}
-
-	/// Create a raw keyboard input from core data.
+	/// Add an input from core mouse-data.
 	#[allow(invalid_value)]
-	fn create_raw_input_keyboard(key_code:u8, key_down:bool) -> INPUT {
-		unsafe {
-			let mut input_record:INPUT = INPUT { type_: 1, u: mem::MaybeUninit::uninit().assume_init() };
-			let flags:u32 = if key_down { 0 } else { KEYEVENTF_KEYUP };
-			let input:KEYBDINPUT = KEYBDINPUT { wVk: key_code as u16, wScan: MapVirtualKeyW(key_code as u32, 0) as u16, dwFlags: flags, time: 0, dwExtraInfo: 0 };
-			ptr::write(&mut input_record.u as *mut _ as *mut KEYBDINPUT, input);
-			input_record
-		}
-	}
-
-	/// Create a raw mouse input from core data.
-	#[allow(invalid_value)]
-	fn create_raw_input_mouse(key_code:u8, key_down:bool) -> INPUT {
-		const MOUSE_DOWN_EVENTS:[u32; 5] = [MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_MIDDLEDOWN, MOUSEEVENTF_XDOWN, MOUSEEVENTF_XDOWN];
-		const MOUSE_UP_EVENTS:[u32; 5] = [MOUSEEVENTF_LEFTUP, MOUSEEVENTF_RIGHTUP, MOUSEEVENTF_MIDDLEUP, MOUSEEVENTF_XUP, MOUSEEVENTF_XUP];
-		let event_list:&[u32; 5] = if key_down { &MOUSE_DOWN_EVENTS } else { &MOUSE_UP_EVENTS };
-
-		unsafe {
-			let mut input_record:INPUT = INPUT { type_: INPUT_MOUSE, u: mem::MaybeUninit::uninit().assume_init() };
-			let flags:u32 = event_list[key_code as usize - 1];
-			let input:MOUSEINPUT = MOUSEINPUT { dx: 0, dy: 0, mouseData: 0, dwFlags: flags, time: 0, dwExtraInfo: 0 };
-			ptr::write(&mut input_record.u as *mut _ as *mut MOUSEINPUT, input);
-			input_record
-		}
+	fn add_raw_mouse_input(&mut self, flags:u32, x:i32, y:i32) {
+		self.inputs.push(
+			InputBuilderInput::MouseInput(
+				unsafe {
+					let mut input_record:INPUT = INPUT { type_: INPUT_MOUSE, u: mem::MaybeUninit::uninit().assume_init() };
+					let input:MOUSEINPUT = MOUSEINPUT { dx: x, dy: y, mouseData: 0, dwFlags: flags, time: 0, dwExtraInfo: 0 };
+					ptr::write(&mut input_record.u as *mut _ as *mut MOUSEINPUT, input);
+					input_record
+				}
+			)
+		);
 	}
 }
 
--- src/mouse.rs
+++ src/mouse.rs
@@ -1,5 +1,5 @@
-use winapi::{ shared::windef::POINT, um::winuser::{ mouse_event, GetCursorPos, SetCursorPos } };
-use crate::{ sleep, keys::LBUTTON };
+use winapi::{ shared::windef::POINT, um::winuser::GetCursorPos };
+use crate::{ InputBuilder, keys::LBUTTON };
 use mini_rand::Randomizable;
 use std::time::Duration;
 
@@ -7,12 +7,12 @@ use std::time::Duration;
 
 /// Move the mouse a specific amount.
 pub fn displace(offset:[i32; 2]) {
-	unsafe { mouse_event(0x01, offset[0] as u32, offset[1] as u32, 0, 0); }
+	InputBuilder::new().with_mouse_displacement(offset).execute();
 }
 
 /// Move the mouse to a specific location.
 pub fn move_to(position:[i32; 2]) {	
-	unsafe { SetCursorPos(position[0], position[1]); };
+	InputBuilder::new().with_mouse_move(position).execute();
 }
 
 /// Get the current position of the mouse, relative to the screen.
@@ -23,18 +23,32 @@ pub fn get_pos() -> [i32; 2] {
 }
 
 /// Click at a specific location without moving the cursor.
-pub fn click<T>(position:[i32; 2], duration:T) where T:Randomizable<Duration> {
+pub fn click<T>(position:[i32; 2], press_duration:T) where T:Randomizable<Duration> {
 	let original_pos:[i32; 2] = get_pos();
-	move_to(position);
-	LBUTTON.send(duration);
-	move_to(original_pos);
+	let press_duration_millis:u64 = press_duration.randomizable_value().as_millis() as u64;
+	InputBuilder::new().with_mouse_move(position).with_send(&LBUTTON, press_duration_millis).with_mouse_move(original_pos).execute();
 }
 
 /// Drag the mouse from one point to another.
 pub fn drag<T>(start:[i32; 2], end:[i32; 2], press_duration:T) where T:Randomizable<Duration> {
-	move_to(start);
-	LBUTTON.press();
-	sleep(press_duration.randomizable_value());
-	move_to(end);
-	LBUTTON.release();
+
+	let mut input_builder:InputBuilder = InputBuilder::new();
+	input_builder.add_mouse_move(start);
+	input_builder.add_press(&LBUTTON);
+
+	let press_duration_millis:u64 = press_duration.randomizable_value().as_millis() as u64;
+	let offset_per_milli:[f32; 2] = [(end[0] - start[0]) as f32 / press_duration_millis.max(1) as f32, (end[1] - start[1]) as f32 / press_duration_millis.max(1) as f32];
+	let mut cursor:[f32; 2] = start.map(|value| value as f32);
+	for index in 0..press_duration_millis {
+		if index > 0 {
+			input_builder.add_delay(1);
+		}
+		input_builder.add_mouse_move(cursor.map(|value| value as i32));
+		cursor[0] += offset_per_milli[0];
+		cursor[1] += offset_per_milli[1];
+	}
+
+	input_builder.add_release(&LBUTTON);
+	input_builder.add_mouse_move(end);
+	input_builder.execute();
 }
